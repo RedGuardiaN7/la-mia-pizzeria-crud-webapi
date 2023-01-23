ba0a1772fcc02bf16d74dd43cab3284f2d9dda4d
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-23 15:54:06 +0100

    Aggiunto il primo API

---
+++ Pizzeria/Controllers/PizzasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pizzeria.Database;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                List<Pizza> list = context.Pizzas.Include(pizza => pizza.Ingredients).ToList<Pizza>();
+                return Ok(list);
+            }
+        }
+    }
+}
--- Pizzeria/Program.cs
+++ Pizzeria/Program.cs
@@ -1,4 +1,8 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
